023088894dba48bf5b958f10c9495916d1b3a83c
kundan223 <kundan0707y@gmail.com>
2025-11-02 14:52:22 +0530

    implement template

--- chaicenter/settings.py
+++ chaicenter/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tweet',
 ]
 
 ROOT_URLCONF = 'chaicenter.urls'
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'chaicenter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
